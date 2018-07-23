1c82705a49f6ee8e8bdf6233cf1b7ee9159648f5
Chris Burgdorff <wesborland1234@aol.com>
2018-07-23 18:53:20 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var MongoClient = require('mongodb').MongoClient;
 var MongoUrl = "mongodb://wesborland1234:vcr357@ds147391.mlab.com:47391/tagtestdb";
 var mongodb = require('mongodb');
 
+app.get('/', function (req, res){
+	res.send("Hello");
+});
+
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.json());
@@ -14,11 +18,11 @@ app.use(bodyParser.json());
 //Connect to Mongo
 var db;
 MongoClient.connect(MongoUrl, (err, client) => {
-	if (err) return console.log(err);
-	db = client.db('tagtestdb')
-	app.listen(process.env.PORT || 3000, function() {
-		console.log("Application is listening on a port");
-	});
+    if (err) return console.log(err)
+    db = client.db('tagtestdb') // whatever your database name is
+    app.listen(process.env.PORT || 3000, function(){
+        console.log("Application is listening on port 3000, or whatever.");
+    });
 });
 
 app.post('/applicant', function (req, res) {
