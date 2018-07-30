6c7540744e524a88a5868821745305a0285e60ee
Chris Burgdorff <wesborland1234@aol.com>
2018-07-29 22:45:32 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var MongoUrl = "mongodb://wesborland1234:fakepassword1@ds147391.mlab.com:47391/tagtestdb";
 var mongodb = require('mongodb');
+var path = require('path');
 
 app.use(express.static(__dirname + "/public"));
 
@@ -22,7 +23,7 @@ MongoClient.connect(MongoUrl, (err, client) => {
 });
 
 app.post('/applicant', function (req, res) {
-	console.log(req.body);
+	//console.log(req.body);
 	db.collection('applicants').insert(req.body, function(err, doc) {
 		res.json(doc);
 	});
@@ -40,9 +41,26 @@ app.put('/applicant/:id', function (req, res) {
   );
 });
 
+app.get('/applicant', function(req, res) {
+	db.collection('applicants').find().toArray(function(err, results){
+		res.json(results);
+	});
+});
+
 app.post('/record', function (req, res) {
-	console.log(req.body);
+	//console.log(req.body);
 	db.collection('records').insert(req.body, function(err, doc){
 		res.json(doc);
+	});
+});
+
+app.get('/admin', function (req, res) {
+	res.sendFile(path.join(__dirname + '/public/applicants.html'));
+});
+
+app.get('/record/:id', function (req, res) {
+	var queryUserId = req.params.id;
+	db.collection('records').find({userId: queryUserId}).toArray(function(err, results){
+		res.json(results);
 	});
 });
